a01680049a6b84fbea93141f09db400b613c668c
Tomasz <lis109@wp.pl>
2022-06-13 17:24:51 +0200

    Initial commit

---
+++ src/main/java/com/example/KangaProjectSpring/KangaProjectSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.KangaProjectSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KangaProjectSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KangaProjectSpringApplication.class, args);
+	}
+
+}
